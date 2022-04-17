ff308427788e3c03110b3e5a63c97bbf101e728d
silveiraoziel <jroziel7@gmail.com>
2022-04-17 15:33:58 -0300

    add project WebChat

---
+++ index.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const inputField = document.getElementById("input");
+    inputField.addEventListener("keydown", (e) => {
+        if (e.code === "Enter") {
+            let input = inputField.value;
+            inputField.value = "";
+            addMessage(input);
+        }
+    });
+});
+
+function addMessage(input) {
+    const messagesContainer = document.getElementById("messages");
+
+    if (document.getElementById("tipoUsuario").value == "cliente") {
+        let clientDiv = document.createElement("div");
+        clientDiv.id = "client"
+        clientDiv.className = "client response"
+        clientDiv.append(input)
+        messagesContainer.appendChild(clientDiv);
+    }
+    else {
+        let clerkDiv = document.createElement("div");
+        clerkDiv.id = "clerk"
+        clerkDiv.className = "clerk response"
+        clerkDiv.append(input)
+        messagesContainer.appendChild(clerkDiv);
+    }
+}
