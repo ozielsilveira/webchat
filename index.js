63a3f57efb4f54781d4ab0f5edbde6ccb3a93b42
silveiraoziel <jroziel7@gmail.com>
2022-04-19 13:55:31 -0300

    add message time

--- index.js
+++ index.js
@@ -12,19 +12,34 @@ document.addEventListener("DOMContentLoaded", () => {
 function addMessage(input) {
     const messagesContainer = document.getElementById("messages");
 
+    let hours = document.createElement("div")
+    const date = new Date();
+    const datenow = date.getHours().toString().padStart(2,"0") + ':' + date.getMinutes().toString().padStart(2,"0"); 
     
-    if (document.getElementById("tipoUsuario").value == "cliente") {
-        let clientDiv = document.createElement("div");
+    if (document.getElementById("tipoUsuario").value == "client") {
+        
+        let clientDiv = document.createElement("div");        
         clientDiv.id = "client"
         clientDiv.className = "client response"
-        clientDiv.append(input)
+        clientDiv.append(input)           
         messagesContainer.appendChild(clientDiv);
+
+        hours.id = "clientHours"
+        hours.className = "clientHours response"
+        hours.append(datenow)
+        messagesContainer.appendChild(hours);
     }
     else {
         let clerkDiv = document.createElement("div");
         clerkDiv.id = "clerk"
         clerkDiv.className = "clerk response"
         clerkDiv.append(input)
+
+        hours.id = "clerkHours"
+        hours.className = "clerkHours response"
+        hours.append(datenow)   
+
         messagesContainer.appendChild(clerkDiv);
+        messagesContainer.appendChild(hours);
     }
 }
